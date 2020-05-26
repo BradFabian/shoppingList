f438d31dba0919fc6b2266b23668fd46f6d3bc82
Brad <bradleofabian@gmail.com>
2020-05-26 15:35:05 -0400

    set up server

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+// Bodyparser Middleware
+app.use(bodyParser.json());
+
+// DB Config
+const db = require("./config/keys").mongoURI;
+//Connect to Mongo
+mongoose.set("useUnifiedTopology", true);
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => console.log(err));
+
+// variable for port
+const port = process.env.PORT || 5000;
+//express listen for port and call back a log //
+app.listen(port, () => console.log(`Server started on PORT ${port}`));
