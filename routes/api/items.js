8eaeda55bbc345950b60b967725675d7af582597
Brad <bradleofabian@gmail.com>
2020-06-09 20:58:28 -0400

    deleted outdated react

---
+++ routes/api/items.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const router = express.Router();
+
+// Item Model
+const Item = require("../../models/Item");
+
+// @route Get api/items
+// @desc GET ALL items
+// @access Public
+
+router.get("/", (req, res) => {
+  Item.find()
+    .sort({ date: -1 })
+    .then((items) => res.json(items));
+});
+
+// @route Post api/items
+// @desc Create a item
+// @access Public
+
+router.post("/", (req, res) => {
+  const newItem = Item({
+    name: req.body.name,
+  });
+
+  newItem.save().then((item) => res.json(item));
+});
+
+// @route Delete api/items/:id
+// @desc Delete a item
+// @access Public
+
+router.delete("/:id", (req, res) => {
+  Item.findById(req.params.id)
+    .then((item) => item.remove().then(() => res.json({ success: true })))
+    .catch((err) => res.status(404).json({ success: false }));
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const items = require("./routes/api/items");
 
 const app = express();
 
@@ -15,6 +16,9 @@ mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("MongoDB Connected..."))
   .catch((err) => console.log(err));
+
+// Use Routes
+app.use("/api/items", items);
 
 // variable for port
 const port = process.env.PORT || 5000;
