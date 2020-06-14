e35ee9fe1c5238346abb49f2cfb9d4014897c138
Brad <bradleofabian@gmail.com>
2020-06-13 23:03:57 -0400

    added shoppinglist component

--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -2,3 +2,30 @@ import React, { Component } from "react";
 import { Container, ListGroup, ListGroupItem, Button } from "reactstrap";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
 import uuid from "uuid";
+
+class ShoppingList extends Component {
+  state = [
+    { id: uuid(), name: "Eggs" },
+    { id: uuid(), name: "Milk" },
+    { id: uuid(), name: "Steaks" },
+    { id: uuid(), name: "Water" },
+  ];
+
+  render() {
+      const { items } = this.state;
+      return(
+          <Container>
+              <Button color="dark" style={{marginBottom: '2rem'}}
+            onClick={() => {
+                const name = prompt('Enter Item');
+                if(name){
+                    this.setState(state => ({items: [..state.items, {id: uuid(), name}]}));
+                }
+            }}
+            >Add Item</Button>
+          </Container>
+      )
+  }
+}
+
+export default ShoppingList;
