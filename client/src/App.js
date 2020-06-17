fe9ddc628525c0b093e308403274838a23d8d4ff
Brad <bradleofabian@gmail.com>
2020-06-16 21:55:12 -0400

    set up boiler plate redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,21 @@
 import React from "react";
 import AppNavbar from "./components/AppNavbar";
 import Shoppinglist from "./components/ShoppingList";
+
+import { Provider } from "react-redux";
+import store from "./store";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <AppNavbar />
-      <Shoppinglist />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <AppNavbar />
+        <Shoppinglist />
+      </div>
+    </Provider>
   );
 }
 
---
+++ client/src/reducers/itemReducer.js
@@ -0,0 +1,10 @@
+import uuid from "uuid";
+
+const initialState = {
+  items: [
+    { id: uuid(), name: "Eggs" },
+    { id: uuid(), name: "Milk" },
+    { id: uuid(), name: "Steaks" },
+    { id: uuid(), name: "Water" },
+  ],
+};
