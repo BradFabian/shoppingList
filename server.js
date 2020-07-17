cefc24d8d02cc92964b130351ad7ab324b066b4e
Brad Fabian <bradfabian@Brads-MacBook-Pro.local>
2020-07-17 16:22:12 -0400

    config set up

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(bodyParser.json());
 
 // DB Config
-const db = require("./config/keys").mongoURI || process.env.mongoURI;
+const db = process.env.mongoURI || require("./config/keys").mongoURI;
 //Connect to Mongo
 mongoose.set("useUnifiedTopology", true);
 mongoose
