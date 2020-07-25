864680cb0326f7ba22209ca9adee3de3313e47a4
Brad <bradleofabian@gmail.com>
2020-07-24 22:00:49 -0400

    added a users post route

--- server.js
+++ server.js
@@ -1,20 +1,19 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
 const path = require("path");
 const items = require("./routes/api/items");
 const users = require("./routes/api/users");
 const app = express();
 
 // Bodyparser Middleware
-app.use(bodyParser.json());
+app.use(express.json());
 // for Dev require("./config/dev").mongoURI//
 // DB Config
 const db = require("./config/dev").mongoURI;
 //Connect to Mongo
 mongoose.set("useUnifiedTopology", true);
 mongoose
-  .connect(db, { useNewUrlParser: true })
+  .connect(db, { useNewUrlParser: true, useCreateIndex: true })
   .then(() => console.log("MongoDB Connected..."))
   .catch((err) => console.log(err));
 
