d44b595f5b1bfd03ebe3b7fbada7c5470a1cd027
Brad Fabian <bradleofabian@gmail.com>
2020-07-27 22:53:27 -0400

    set up auth and error actions

--- client/src/App.js
+++ client/src/App.js
@@ -5,22 +5,28 @@ import ItemModal from "./components/ItemModal";
 import { Container } from "reactstrap";
 import { Provider } from "react-redux";
 import store from "./store";
+import { loadUser } from "./actions/authActions";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
-function App() {
-  return (
-    <Provider store={store}>
-      <div className="App">
-        <AppNavbar />
-        <Container>
-          <ItemModal />
-          <Shoppinglist />
-        </Container>
-      </div>
-    </Provider>
-  );
+class App extends React.Component {
+  componentDidMount() {
+    store.dispatch(loadUser());
+  }
+  render() {
+    return (
+      <Provider store={store}>
+        <div className="App">
+          <AppNavbar />
+          <Container>
+            <ItemModal />
+            <Shoppinglist />
+          </Container>
+        </div>
+      </Provider>
+    );
+  }
 }
 
 export default App;
